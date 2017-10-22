46712a24a0467b95123a268e2b38126d4b7865cc
Jimmy Zhen <jzhen@highwire.org>
2017-10-22 00:56:23 -0700

    Refactored to load sensitive configs from .env, then merge default and local (if any) configs

--- src/mysql/config.js
+++ src/mysql/config.js
@@ -1,28 +1,32 @@
-const env = require('dotenv')
+const merge = require('lodash/merge')
 
-const templatePath =
-`${__dirname}/../template-files/.env_template`
+/**
+ * Load .env settings into process.env
+ * Will fail silently if no .env file present
+ */
+require('dotenv').config()
 
-const getTemplateConfig = () =>
-  env.config({ path: templatePath }).parsed
+/**
+ * Get default configs
+ */
+const config = require('./env/default')
 
-const getConfig = () =>
-  env.config().parsed
-
-const backupConfig = {
-  port: 8080,
-  allowCrossDomain: false,
-  https: false,
-  mysql_host: 'localhost',
-  mysql_user: 'root',
-  mysql_password: '',
-  mysql_database: '',
-  mysql_soft_delete: false,
-  mysql_soft_delete_field: 'deleted',
-  mysql_query_debug: false,
-  mysql_port: 3306
+/**
+ * Get local configs
+ */
+let localConfig
+try {
+  // Local configs may not exist
+  localConfig = require(`./env/${config.env}`)
+  localConfig = localConfig || {}
+} catch (err) {
+  localConfig = {}
 }
 
-const config = getConfig() || getTemplateConfig() || backupConfig
+/**
+ * Merge the config files
+ * localConfig will override defaults
+ */
+merge({}, config, localConfig)
 
 export default config
